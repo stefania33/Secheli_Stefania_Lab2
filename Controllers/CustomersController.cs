df1ec704a519a560cdcf933c887893f6f1c4454c
stefania33 <58682871+stefania33@users.noreply.github.com>
2020-11-29 15:13:55 +0200

    Add files via upload

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,24 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LibraryModel.Data;
 using LibraryModel.Models;
 
-using System.Net.Http;
-using System.Text;
-using Newtonsoft.Json;
-
-
-namespace Secheli_Stefania_Lab2.Controllers
-{ 
+namespace LibraryWebAPI.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class CustomersController : Controller
+    public class CustomersController : ControllerBase
     {
         private readonly LibraryContext _context;
-        private string _baseUrl = "http://localhost:57387/api/Customers";
 
         public CustomersController(LibraryContext context)
         {
@@ -126,132 +121,5 @@ namespace Secheli_Stefania_Lab2.Controllers
         {
             return _context.Customers.Any(e => e.CustomerID == id);
         }
-
-        public async Task<ActionResult> Index()
-        {
-            var client = new HttpClient();
-            var response = await client.GetAsync(_baseUrl);
-            if (response.IsSuccessStatusCode)
-            {
-                var customers = JsonConvert.DeserializeObject<List<Customer>>(await response.Content.
-                ReadAsStringAsync());
-                return View(customers);
-            }
-            return NotFound();
-        }
-
-        // GET: Inventory/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new BadRequestResult();
-            }
-            var client = new HttpClient();
-            var response = await client.GetAsync($"{_baseUrl}/{id.Value}");
-            if (response.IsSuccessStatusCode)
-            {
-                var customer = JsonConvert.DeserializeObject<Customer>(
-                await response.Content.ReadAsStringAsync());
-                return View(customer);
-            }
-            return NotFound();
-        }
-        // GET: Customers/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind("CustomerID,Name,Adress,BirthDate")] Customer customer)
-        {
-            if (!ModelState.IsValid) return View(customer);
-            try
-            {
-                var client = new HttpClient();
-                string json = JsonConvert.SerializeObject(customer);
-                var response = await client.PostAsync(_baseUrl,
-                new StringContent(json, Encoding.UTF8, "application/json"));
-                if (response.IsSuccessStatusCode)
-                {
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError(string.Empty, $"Unable to create record: {ex.Message}");
-            }
-            return View(customer);
-        }
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new BadRequestResult();
-            }
-            var client = new HttpClient();
-            var response = await client.GetAsync($"{_baseUrl}/{id.Value}");
-            if (response.IsSuccessStatusCode)
-            {
-                var customer = JsonConvert.DeserializeObject<Customer>(
-                await response.Content.ReadAsStringAsync());
-                return View(customer);
-            }
-            return new NotFoundResult();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind("CustomerID,Name,Adress,BirthDate")] Customer customer)
-        {
-            if (!ModelState.IsValid) return View(customer);
-            var client = new HttpClient();
-            string json = JsonConvert.SerializeObject(customer);
-            var response = await client.PutAsync($"{_baseUrl}/{customer.CustomerID}",
-            new StringContent(json, Encoding.UTF8, "application/json"));
-            if (response.IsSuccessStatusCode)
-            {
-                return RedirectToAction("Index");
-            }
-            return View(customer);
-        }
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new BadRequestResult();
-            }
-            var client = new HttpClient();
-            var response = await client.GetAsync($"{_baseUrl}/{id.Value}");
-            if (response.IsSuccessStatusCode)
-            {
-                var customer = JsonConvert.DeserializeObject<Customer>(await response.Content.ReadAsStringAsync());
-                return View(customer);
-            }
-            return new NotFoundResult();
-        }
-        // POST: Customers/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Delete([Bind("CustomerID")] Customer customer)
-        {
-            try
-            {
-                var client = new HttpClient();
-                HttpRequestMessage request =
-                new HttpRequestMessage(HttpMethod.Delete, $"{_baseUrl}/{customer.CustomerID}")
-                {
-                    Content = new StringContent(JsonConvert.SerializeObject(customer), Encoding.UTF8, "application/json")
-                };
-                var response = await client.SendAsync(request);
-                return RedirectToAction("Index");
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError(string.Empty, $"Unable to delete record: {ex.Message}");
-            }
-            return View(customer);
-        }
     }
-
 }

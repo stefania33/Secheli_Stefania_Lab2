81d3eeb74c5ba45a694250a755b3749f05e56116
stefania33 <58682871+stefania33@users.noreply.github.com>
2020-11-29 15:12:29 +0200

    Add files via upload

---
+++ LibraryModel/Data/LibraryContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryModel.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LibraryModel.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<PublishedBook> PublishedBooks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Book>().ToTable("Book");
+            modelBuilder.Entity<Publisher>().ToTable("Publisher");
+            modelBuilder.Entity<PublishedBook>().ToTable("PublishedBook");
+            modelBuilder.Entity<PublishedBook>()
+            .HasKey(c => new { c.BookID, c.PublisherID }); //cheie primara compusa
+        }
+
+    }
+}
---
+++ LibraryModel/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryModel.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+        public ICollection<PublishedBook> PublishedBooks { get; set; }
+    }
+}
+

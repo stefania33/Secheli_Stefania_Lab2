8ba703c2f871e6cc9590e5ee3e8b715fa138d70d
stefania33 <58682871+stefania33@users.noreply.github.com>
2020-11-29 15:13:15 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,32 +6,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using LibraryModel.Data;
-using Secheli_Stefania_Lab2.Data;
 
-namespace Secheli_Stefania_Lab2
+namespace LibraryWebAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<LibraryContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
